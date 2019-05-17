a760be099ebc279ab0771694521707d6718fc614
aburk3 <aburk3@gmail.com>
2019-05-17 12:27:29 -0500

    Added condition so that if no ingredients exist, show a message where the ingredients would normally show

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,23 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    // Array(3) gives array with 3 elements
-    console.log(igKey);
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      console.log(i);
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // Array(3) gives array with 3 elements
+      console.log(igKey);
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        console.log(i);
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  console.log(transformedIngredients);
 
   return (
     <div className={classes.Burger}>
