a877de881c86ea41c219a00eaa52b1a4172bfbde
aburk3 <aburk3@gmail.com>
2019-06-06 05:21:08 -0500

    Added handlers for when the user clicks on the cancel or continue buttons

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,12 +3,12 @@ import React from "react";
 import classes from "./Button.module.css";
 
 const Button = props => (
-  <Button
+  <button
     className={[classes.Button, classes[props.btnType]].join(" ")}
     onClick={props.clicked}
   >
     {props.children}
-  </Button>
+  </button>
 );
 
 export default Button;
