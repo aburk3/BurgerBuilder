37d17c25081c71117d073d83fd935d2e63df687f
aburk3 <aburk3@gmail.com>
2019-06-06 05:35:32 -0500

    Added stylesheet for tool bar and added Toolbar component to the Layout component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import classes from "./Toolbar.module.css";
+
 const Toolbar = props => (
-  <header>
+  <header className={classes.Toolbar}>
     <div>MENU</div>
     <div>LOGO</div>
     <nav>...</nav>
