48c722f239e5136b66681cd48e00d1b4b4993c67
aburk3 <aburk3@gmail.com>
2019-07-06 15:15:53 -0500

    Added Burger component to CheckoutSummary and modified stylesheet to be a module

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const checkoutSummary = props => {
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+
+const CheckoutSummary = props => {
   return (
     <div>
       <h1>We hope it tastes well!</h1>
+      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
     </div>
   );
 };
