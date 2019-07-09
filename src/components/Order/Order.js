56a550401ea385c35eacebd6cfc98e90a7c84a7d
aburk3 <aburk3@gmail.com>
2019-07-09 04:53:45 -0500

    Create new ingredients array

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,13 +2,24 @@ import React from "react";
 
 import classes from "./Order.module.css";
 
-const Order = props => (
-  <div className={classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>
-      Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
-    </p>
-  </div>
-);
+const Order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: Salad (1)</p>
+      <p>
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default Order;
