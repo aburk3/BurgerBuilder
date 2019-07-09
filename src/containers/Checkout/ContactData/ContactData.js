41d06c371402c9bb805626290ae5bf5c74a5138f
aburk3 <aburk3@gmail.com>
2019-07-09 06:07:11 -0500

    Added formData variable for saving the contact form contents to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -66,10 +66,20 @@ class ContactData extends Component {
   orderHandler = event => {
     event.preventDefault();
     this.setState({ loading: true });
+
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value;
+    }
+
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     };
+
     axios
       .post("/orders.json", order)
       .then(response => {
@@ -103,7 +113,7 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form action="">
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
